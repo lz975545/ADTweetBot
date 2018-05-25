adbb7ebcddb93b33d2f8e0e0b1d2bf6fd156cfbc
Lukas Zamora <lzamora975545@gmail.com>
2018-05-25 14:21:19 -0500

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,5 +1,4 @@
 # Obtain keys at apps.twitter.com and create an app.
-
 consumer_key = 'YOUR_CONSUMER_KEY_HERE'
 consumer_secret = 'YOUR_CONSUMER_SECRET_HERE'
 access_token = 'YOUR_ACCESS_TOKEN_HERE'
