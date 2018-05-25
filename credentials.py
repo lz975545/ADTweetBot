e09efe402492d28534f2ba0247feeef3e9a73fc8
Lukas Zamora <lzamora975545@gmail.com>
2018-05-24 20:17:53 -0500

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-consumer_key = 'YOUR_CONSUMER_KEY'
-consumer_secret = 'YOUR_CONSUMER_SECRET'
-access_token = 'YOUR_ACCESS_TOKEN'
-access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
+consumer_key = 'OgQ77MPrw9o7SDH0twblVlRGU'
+consumer_secret = 'swY2L62ylI1gmkoUZ4AL9kYYFxta5iGNDPm8d6X26RouEOa2LM'
+access_token = '999151101176877057-TxXgXxVdbUvGHLgx55uA2Qq8DjLIk8c'
+access_token_secret = 'Mc9yFUtBucQ7nMq6102rUCzwpyt1udODbCzbYYSIRWvXs'
