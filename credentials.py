945cdc1b73e1692978bc922c3a5cc9ef4bce893c
Lukas Zamora <lzamora975545@gmail.com>
2018-05-27 00:27:13 -0500

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,5 +1,5 @@
 # Obtain keys at apps.twitter.com and create an app.
-consumer_key = 'OgQ77MPrw9o7SDH0twblVlRGU'
-consumer_secret = 'swY2L62ylI1gmkoUZ4AL9kYYFxta5iGNDPm8d6X26RouEOa2LM'
-access_token = '999151101176877057-TxXgXxVdbUvGHLgx55uA2Qq8DjLIk8c'
-access_token_secret = 'Mc9yFUtBucQ7nMq6102rUCzwpyt1udODbCzbYYSIRWvXs'
+consumer_key = '2xmHKxfqcKnipXnTuVfsWIz7x'
+consumer_secret = 'Rb7zypsYFCg6F4MhTHQHUDIO9CZqWXkeFP0oUc6DD6lF2Cj7ZG'
+access_token = '999151101176877057-Emzw4oqXw9tXamEnOb2IBl96NfMEp5n'
+access_token_secret = 'RPOVy9EduEW4oq1AOsVYhunVmDGeUN96udIr60DaJRCj1'
